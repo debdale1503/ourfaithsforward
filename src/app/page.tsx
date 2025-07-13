a3fcf320a954f2a8f6fcb9ed7284b31ad6af9dcf
debdale1503 <debdale1503@gmail.com>
2025-07-12 19:30:08 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ import { Video } from "../components/Video";
 import { Testimonials } from "../components/Testimonials";
 import { Faq } from "../components/Faq";
 import { Cta } from "../components/Cta";
-import { benefitOne, benefitTwo } from "../components/data";
 
 export default function Home() {
   return (
